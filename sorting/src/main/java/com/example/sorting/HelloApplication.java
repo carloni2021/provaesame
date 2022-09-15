5cbd47eed339f297bb50479d40124c7a1f437620
carloni2021 <92363079+carloni2021@users.noreply.github.com>
2022-09-15 15:07:28 +0200

    Add files via upload

---
+++ sorting/src/main/java/com/example/sorting/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.example.sorting;
+
+public class HelloApplication{
+
+    public static void main(String[] args) {
+        int numero = 1;
+        if(numero%2==0){
+            System.out.println("Il numero inserito risulta pari");
+        }else{
+            System.out.println("Il numero inserito risulta dispari");
+        }
+
+    }
+}
